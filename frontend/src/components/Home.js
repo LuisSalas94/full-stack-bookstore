4013e6418abdbe2f8e5c5c23e623a7ef67329615
Fernando Salas <luisfernandosalasg@gmail.com>
2022-10-22 16:26:49 -0500

    Add Footer component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,6 +6,7 @@ import Features from "./ui/Features";
 import Cards from "./Cards";
 import Feature2 from "./ui/Feature2";
 import FAQ from "./ui/FAQ";
+import Footer from "./ui/Footer";
 
 const Home = () => {
 	return (
@@ -15,6 +16,7 @@ const Home = () => {
 			<Cards />
 			<Feature2 />
 			<FAQ />
+			<Footer />
 			{/* <Card /> */}
 		</div>
 	);
