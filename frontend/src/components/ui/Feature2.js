3ad4625796acd690379d39bc71adcc5269a6c6ae
Fernando Salas <luisfernandosalasg@gmail.com>
2022-10-24 12:18:38 -0500

    Refactor Feature2 Component

--- frontend/src/components/ui/Feature2.js
+++ frontend/src/components/ui/Feature2.js
@@ -44,16 +44,17 @@ const Feature2 = () => {
 						<div className="mr-4">
 							<div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
 								<svg
-									className="w-8 h-8 text-purple-400"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
 									stroke="currentColor"
-									viewBox="0 0 52 52"
+									className="w-6 h-6 text-purple-400"
 								>
-									<polygon
-										strokeWidth="3"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										fill="none"
-										points="29 13 14 29 25 29 23 39 38 23 27 23"
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
 									/>
 								</svg>
 							</div>
@@ -71,16 +72,17 @@ const Feature2 = () => {
 						<div className="mr-4">
 							<div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
 								<svg
-									className="w-8 h-8 text-purple-400"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
 									stroke="currentColor"
-									viewBox="0 0 52 52"
+									className="w-6 h-6 text-purple-400"
 								>
-									<polygon
-										strokeWidth="3"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										fill="none"
-										points="29 13 14 29 25 29 23 39 38 23 27 23"
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
 									/>
 								</svg>
 							</div>
